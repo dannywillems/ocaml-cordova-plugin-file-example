e08601184fe15975df52957cd227fcfd67ea5e59
Danny Willems <contact@danny-willems.be>
2016-06-17 15:07:26 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -6,19 +6,26 @@ let create_paragraph str =
   p
 
 let on_device_ready _ =
-  let f = Cordova_file.t () in
-  Dom.appendChild doc##.body (create_paragraph (f#application_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#application_storage_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#data_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#cache_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#external_application_storage_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#external_data_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#external_cache_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#external_root_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#temp_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#synced_data_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#documents_directory));
-  Dom.appendChild doc##.body (create_paragraph (f#shared_directory))
+  Dom.appendChild doc##.body (create_paragraph
+  (Cordova_file.application_directory ()));
+  Dom.appendChild doc##.body (create_paragraph
+  (Cordova_file.application_storage_directory ()));
+  Dom.appendChild doc##.body (create_paragraph (Cordova_file.data_directory ()));
+  Dom.appendChild doc##.body (create_paragraph (Cordova_file.cache_directory ()));
+  Dom.appendChild doc##.body (create_paragraph
+  (Cordova_file.external_application_storage_directory ()));
+  Dom.appendChild doc##.body (create_paragraph
+  (Cordova_file.external_data_directory ()));
+  Dom.appendChild doc##.body (create_paragraph
+  (Cordova_file.external_cache_directory ()));
+  Dom.appendChild doc##.body (create_paragraph
+  (Cordova_file.external_root_directory ()));
+  Dom.appendChild doc##.body (create_paragraph (Cordova_file.temp_directory ()));
+  Dom.appendChild doc##.body (create_paragraph
+  (Cordova_file.synced_data_directory ()));
+  Dom.appendChild doc##.body (create_paragraph (Cordova_file.documents_directory
+  ()));
+  Dom.appendChild doc##.body (create_paragraph (Cordova_file.shared_directory ()))
 
 let _ =
   Cordova.Event.device_ready on_device_ready
